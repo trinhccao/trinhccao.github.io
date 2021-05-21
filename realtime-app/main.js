fbb256c4e29c37ef3e32d9566526d393e758b60e
Trinh <t.trinh.it@gmail.com>
2021-05-22 00:57:17 +0700

    Update main.js

--- realtime-app/main.js
+++ realtime-app/main.js
@@ -1,6 +1,6 @@
 'use strict';
 const heading = document.querySelector('h1');
-const socket = io('ws://14.232.232.109:8888');
+const socket = io('14.232.232.109:8888');
 
 socket.on('connect', function() {
   heading.textContent = 'Connection is opened';
