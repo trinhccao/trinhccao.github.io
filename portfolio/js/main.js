2b2b8ef251921a2b18005fd0cb30c7bb6d0585da
Trinh <t.trinh.it@gmail.com>
2021-05-29 09:57:21 +0700

    test

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,2 +1,11 @@
 'use strict';
 console.log('JS loaded OK');
+
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    console.log(this.responseText);
+  }
+}
+xhr.open('GET', './demo.json', true);
+xhr.send();
